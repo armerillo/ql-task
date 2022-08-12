14b61d0422fb3ee76228384a7f8e89ff2ccc8cc7
geraldanosike <geraldanosike.javhse@gmail.com>
2022-08-12 21:20:47 +0100

    store

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -5,7 +5,6 @@ const { MemoryStore } = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 1000,
   max: 3,
-  //message: "Your limit exceeded",
   standardHeaders: true,
   legacyHeaders: false,
   handler: function (req, res) {
@@ -17,40 +16,3 @@ const limiter = rateLimit({
 
 });
 module.exports = limiter;
-
-
-/*const dotenv = require("dotenv");
-const redis = require("redis");
-const RateLimit = require("express-rate-limit");
-const RedisStore = require("rate-limit-redis");
-
-dotenv.config();
-
-// const redisClient = redis.createClient(
-//   process.env.redis_host,
-//   process.env.redis_port,
- 
-// );
-
-const redisClient = redis.createClient(process.env.redisendpointUri, { password:process.env.redispassword });
-
-
-redisClient.on("error", function (err) {
-  console.log(redis_host + ":"  + err);
-});
-redisClient.on("connect", function () {
-  console.log("Redis server connected -- " + redis_host + "" );
-});
-
-const limiter =  RateLimit({
-  store: new RedisStore({
-    client: redisClient,
-    expiry: 10,
-  }),
-  max: 10,
-  windowMs: 10 * 1000,
-});
-
-module.exports = limiter;
-
-*/
