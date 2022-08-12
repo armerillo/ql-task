a013d7d98cc4ddc9834cfe1205ea7164433b9f1a
geraldanosike <geraldanosike.javhse@gmail.com>
2022-08-12 17:08:40 +0100

    rate limit

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,15 +1,12 @@
 const rateLimit = require("express-rate-limit");
 
-
-// Rate limiting
-
-  const limiter = rateLimit({
-    windowMs: 1000, // 1 second
-    max: 3, // limit each IP to 3 requests per windowMs
+const limiter = rateLimit({
+    windowMs: 1000,
+    max: 3,
     message: "Your limit exceeded",
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  });
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 
 
 module.exports = limiter;
